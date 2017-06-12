48e07e8394bcc275e6a717d7330512a6b35914e4
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-06-12 13:17:49 +0300

    Refactor logging

--- cogsciabc/log.py
+++ cogsciabc/log.py
@@ -6,25 +6,18 @@ logger = logging.getLogger(__name__)
 def logging_setup():
     """ Set logging
     """
-    logger.setLevel(logging.INFO)
-    model_logger = logging.getLogger("sdirl")
-    model_logger.setLevel(logging.INFO)
-    model_logger.propagate = False
-    elfi_logger = logging.getLogger("elfi")
-    elfi_logger.setLevel(logging.INFO)
-    elfi_logger.propagate = False
-    elfi_methods_logger = logging.getLogger("elfi.methods")
-    elfi_methods_logger.setLevel(logging.DEBUG)
-    elfi_methods_logger.propagate = False
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-
     ch = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
-    model_logger.handlers = [ch]
-    elfi_logger.handlers = [ch]
-    elfi_methods_logger.handlers = [ch]
-    logger.handlers = [ch]
 
+    _set_log("cogsciabc", logging.INFO, [ch])
+    _set_log("elfi", logging.INFO, [ch])
+    _set_log("elfie", logging.INFO, [ch])
+    _set_log("elfirl", logging.INFO, [ch])
+
+def _set_log(name, level, handlers):
+    l = logging.getLogger(name)
+    l.setLevel(level)
+    l.propagate = False
+    l.handlers = handlers
 
