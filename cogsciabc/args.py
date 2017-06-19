8e0d0017e7325b0e8cda473be06e4e682139ea11
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-06-19 10:40:58 +0300

    Fix parsing of seed from args

--- cogsciabc/args.py
+++ cogsciabc/args.py
@@ -3,7 +3,7 @@ import sys
 def parse_args():
     args = dict()
     if len(sys.argv) > 1:
-        args["seed"] = sys.argv[1]
+        args["seed"] = int(sys.argv[1])
     else:
         args["seed"] = 0
     return args
