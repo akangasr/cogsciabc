39cdd9c858c6c77ad5c82a42012953a8ffd9a80b
Antti Kangasrääsiö <antti.kangasraasio@hiit.fi>
2017-09-21 09:12:35 +0300

    Adjust scripts

--- generate_slurm_commands.py
+++ generate_slurm_commands.py
@@ -5,48 +5,25 @@ script_name = "./elfie/slurm/run_experiment_slurm.sh"
 repl_start = 1
 n_replicates = 10
 seed_modulo = 10000000
-#methods = ["grid", "lbfgsb", "neldermead", "bo"]
 methods = ["grid", "neldermead", "bo"]
 scripts = {
 "cogsciabc/cogsciabc/run_learningmodel.py": {
     "id": "le",
-    "samples": [16, 81, 256, 625, 1296, 2401],
-    "time": {16: "0-00:30:00",
-             81: "0-00:30:00",
-             256: "0-01:00:00",
-             625: "0-02:00:00",
-             1296: "0-04:00:00",
-             2401: "0-08:00:00"},
-    "mem": {16: 1000,
-            81: 1000,
-            256: 2000,
-            625: 3000,
-            1296: 5000,
-            2401: 8000},
-    "cores": 11,
+    "samples": {"grid": [16, 81, 256, 625],
+                "neldermead": [10, 20, 30, 50, 70, 110, 150, 190],
+                "bo": [25, 50, 75, 100, 150, 200, 250]},
+    "cores": 2,
     "scale": {16: 2,
               81: 3,
               256: 4,
               625: 5,
-              1296: 6,
-              2401: 7},
+              1296: 6},
     },
 "cogsciabc/cogsciabc/run_menumodel.py": {
     "id": "me",
-    "samples": [27, 64, 125, 216, 343, 512],
-    "time": {27:  "1-00:00:00",
-             64:  "1-00:00:00",
-             125: "2-00:00:00",
-             216: "3-00:00:00",
-             343: "4-00:00:00",
-             512: "5-00:00:00"},
-    "mem": {27:  4000,
-            64:  4000,
-            125: 4000,
-            216: 5000,
-            343: 5000,
-            512: 6000},
-    "cores": 21,
+    "samples": {"grid": [27, 64, 125, 216, 343],
+                "neldermead": [10, 20, 30],
+                "bo": [20, 40, 60, 80, 100, 160, 240, 300]},
     "scale": {27: 3,
               64: 4,
               125: 5,
@@ -57,29 +34,38 @@ scripts = {
 }
 
 for script, params in scripts.items():
-    for method in methods:
-        if params["id"] == "me" and method not in ["bo", "grid"]:
-            continue
-        for samples in params["samples"]:
-            if params["id"] == "le" and method != "grid" and samples > 1000:
-                continue
+    for method, samplesl in params["samples"].items():
+        for samples in samplesl:
             for rep in range(repl_start-1, n_replicates):
-                time = params["time"][samples]
-                cores = params["cores"]
-                if True or method in ["lbfgsb", "neldermead"]:
-                    cores = 2  # not parallel
+                if params["id"] == "me":
+                    cores = 21
+                    mem = 5000
+                    if samples < 100:
+                        time = "1-00:00:00"
+                    elif samples < 200:
+                        time = "2-00:00:00"
+                    elif samples < 300:
+                        time = "3-00:00:00"
+                    else:
+                        time = "4-00:00:00"
+                    if method == "neldermead":
+                        time = "5-00:00:00"
+                        cores = 2
+                if params["id"] == "le":
+                    cores = 2
+                    mem = 3000
                     if samples < 100:
                         time = "0-04:00:00"
                     elif samples < 300:
                         time = "0-08:00:00"
                     elif samples < 1000:
                         time = "1-00:00:00"
-                    elif samples < 1500:
-                        time = "1-12:00:00"
                     else:
-                        time = "2-00:00:00"
-                mem = params["mem"][samples]
-                scale = params["scale"][samples]
+                        time = "1-12:00:00"
+                if samples in params["scale"]:
+                    scale = params["scale"][samples]
+                else:
+                    scale = 1
                 identifier = "{}_{}_{:02d}_{:02d}"\
                         .format(params["id"], method, samples, rep+1)
                 hsh = hashlib.sha224(bytearray(identifier, 'utf-8')).digest()
